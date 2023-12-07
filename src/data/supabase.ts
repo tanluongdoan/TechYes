7d1a7ec98dbbb54b7973e603aac18213d0d8cc77
dbvu99 <duc@bluespacecreative.com>
2023-12-07 12:14:51 -0600

    updating grid layout

--- src/data/supabase.ts
+++ src/data/supabase.ts
@@ -4,4 +4,10 @@ const supabaseUrl = "https://pfourseopabbkzxtwolb.supabase.co";
 const supabaseKey = import.meta.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+export const getProducts = async () => {
+  const { data } = await supabase.from("CPUProducts").select("*");
+
+  return data || [];
+};
+
 export default supabase;
