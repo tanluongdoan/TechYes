7bbd0ff591757658ca67f030a3855271c00a109a
dbvu99 <duc@bluespacecreative.com>
2023-12-07 22:57:20 -0600

    updating tech yes

--- src/data/supabase.ts
+++ src/data/supabase.ts
@@ -1,13 +1,65 @@
 import { createClient } from "@supabase/supabase-js";
+import { slugify } from "@util/helpers";
 
 const supabaseUrl = "https://pfourseopabbkzxtwolb.supabase.co";
 const supabaseKey = import.meta.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export const getProducts = async () => {
-  const { data } = await supabase.from("CPUProducts").select("*");
+  const promises = [
+    "CPUProducts",
+    "GPUProducts",
+    "MOBOProducts",
+    "RAMProducts",
+    "SSDProducts",
+  ].map((item) => supabase.from(item).select("*"));
 
-  return data || [];
+  const results = (await Promise.allSettled(promises)).flatMap((item) => {
+    if (item.status === "fulfilled" && item.value.data?.length) {
+      return item.value.data.map((product) => {
+        const {
+          ProductCode,
+          ProductTitle,
+          FeaturedImage,
+          SortPrice,
+          ProductCategory,
+          PriceInclVAT,
+        }: any = product;
+
+        return {
+          id: ProductCode,
+          slug: slugify(ProductTitle),
+          body: "",
+          collection: "product",
+          data: {
+            // title: ProductTitle,
+            title: ProductTitle,
+            id: ProductCode,
+            intro: "",
+            // intros: [SocketType, CPUCores],
+            thumbnail: FeaturedImage,
+            price: SortPrice,
+            priceDisplay: PriceInclVAT,
+            offer_price: 0,
+            taxable: true,
+            tax_included: false,
+            in_stock: true,
+            is_digital: false,
+            shipping: { weight: 649 },
+            category: [ProductCategory, "All"],
+            date: "2023-08-28T20:42:48.308Z",
+            style: { container: "md" },
+          },
+        };
+      });
+    }
+
+    return [];
+  });
+
+  //   console.log(results);
+
+  return results || [];
 };
 
 export default supabase;
